d271ea39b7840563dec48f0ca4aec4273aed74fe
fundakol <22340156+fundakol@users.noreply.github.com>
2022-03-13 20:40:27 +0100

    Refactor tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,16 @@
 import pytest
+import os
 
 from tests.mock_server import MockServer
+
+
+@pytest.fixture(scope='session', autouse=True)
+def environment():
+    os.environ['XRAY_API_BASE_URL'] = 'http://127.0.0.1:5002'
+    os.environ['XRAY_API_USER'] = 'jirauser'
+    os.environ['XRAY_API_PASSWORD'] = 'jirapassword'
+    os.environ['XRAY_CLIENT_ID'] = 'client_id'
+    os.environ['XRAY_CLIENT_SECRET'] = 'client_secret'
 
 
 @pytest.fixture(scope='session', autouse=True)
